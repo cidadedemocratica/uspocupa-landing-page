3825c62287526d2a572f8f3b44d18ec70180b328
Marcos Lopes <marcosmlopes01@gmail.com>
2012-08-01 10:38:42 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UspocupaLandingPage::Application.config.secret_token = '926ab0214b436ba83ae2c0ca33d5370f5f2abb784901ffcbcbc30d30e98db5a79fa7d9bae084becc60a75e746c43dd95fb8bd4c8cb7452607acdc1f7002facb6'
